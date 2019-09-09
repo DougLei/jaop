e6d22d5b71bc6426410992c5c39850f933096a2f
DougLei <953034206@qq.com>
2019-09-09 16:52:56 +0800

    t

--- src/main/java/com/douglei/aop/IgnoreInterfacesProperties.java
+++ src/main/java/com/douglei/aop/IgnoreInterfacesProperties.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.douglei.tools.instances.reader.ResourcesReader;
+import com.douglei.tools.instances.file.resources.reader.ResourcesReader;
 import com.douglei.tools.utils.reflect.ClassLoadUtil;
 
 /**
